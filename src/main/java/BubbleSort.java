cbca10cd223cf4af9c622605dd383e2b01d64238
Nick Edwards <nicholas6598@gmail.com>
2019-10-20 13:37:26 -0400

    Made BubbleSort

---
+++ src/main/java/BubbleSort.java
@@ -0,0 +1,59 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class BubbleSort {
+
+    private void BubbleSort(int[] A) {
+        int BOUND = A.length;
+        for (int i = 0; i < BOUND-1; i++)
+            for (int j = 0; j < BOUND-i-1; j++)
+                if (A[j] > A[j+1])
+                {
+                    // swap arr[j+1] and arr[i]
+                    int temp = A[j];
+                    A[j] = A[j+1];
+                    A[j+1] = temp;
+                }
+    }
+
+    public static void main (String [] args){
+        BubbleSort sort = new BubbleSort();
+
+        try {
+            File f = new File(args[0]);
+            int[] array = sort.getValues(f);
+            sort.BubbleSort(array);
+            sort.print(array);
+        }catch(IOException e){e.printStackTrace();}
+    }
+
+    private int[] getValues(File f) throws IOException {
+        Scanner fScanner = new Scanner(f);
+        ArrayList<Integer> arr = new ArrayList<>();
+        while (fScanner.hasNextInt()) {
+            arr.add(fScanner.nextInt());
+        }
+
+        int[] array = new int[arr.size()];
+        for (int k = 0; k < arr.size(); k++)
+            array[k] = arr.get(k);
+        fScanner.close();
+        return array;
+    }
+
+    private void print(int[] A){
+        for(int k: A){
+            System.out.print(k);
+            if(k!=A[A.length-1])
+                System.out.print(" ");
+        }
+    }
+
+    private static void swap(int[] A, int j, int i){
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+}
